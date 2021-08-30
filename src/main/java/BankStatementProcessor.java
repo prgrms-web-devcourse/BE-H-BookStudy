4092065389adfeb7777c66a5de9bb169ea4ccc9f
minhee <queen.minhee@gmail.com>
2021-08-30 10:38:11 +0900

    2. 새로 배운거 - stream.summaryStatistics()의 존재 - DoubleSummaryStatistics (java.util에 있고 통게자료를 내주는 게 있다는 점) - Functional interface의 활용 (summarizeTransactions(), findTransactions() 주목)   - 함수형 인터페이스를 사용함으로써 코드량이 대폭 줄었다.

--- src/main/java/BankStatementProcessor.java
+++ src/main/java/BankStatementProcessor.java
@@ -1,9 +1,10 @@
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 
-// 여기서는 계산하는 역할만 맡는다!
-// 모든 연산에서 메서드 인수인 입출금 내역 목록을 공유하니까 이를 클래스의 필드로 만든다.
+//0830. functional interface 사용으로 모두 변경해보기
 public class BankStatementProcessor {
     private final List<BankTransaction> bankTransactions;
 
@@ -11,45 +12,37 @@ public class BankStatementProcessor {
         this.bankTransactions = bankTransactions;
     }
 
+    public SummaryStatistics summarizeTransactions(){
+        final DoubleSummaryStatistics doubleSummaryStatistics = bankTransactions.stream()
+                .mapToDouble(BankTransaction::getAmount)
+                .summaryStatistics();
+
+        return new SummaryStatistics(doubleSummaryStatistics.getSum(),
+                doubleSummaryStatistics.getMax(),
+                doubleSummaryStatistics.getMin(),
+                doubleSummaryStatistics.getAverage());
+    }
+
     public double calculateTotalAmount() {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            total += bankTransaction.getAmount();
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransactions) -> acc+bankTransactions.getAmount());
     }
 
     public double calculateTotalInMonth(final Month month) {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDate().getMonth() == month) {
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransactions) ->
+                bankTransactions.getDate().getMonth() == month ? acc + bankTransactions.getAmount() : acc);
     }
 
     public double calculateTotalForCategory(final String category) {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDescription().equals(category)) {
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransactions) -> bankTransactions.getDescription().equals(category) ?
+                acc + bankTransactions.getAmount() : acc);
     }
 
     //특정 날짜 범위
     public double calculateTotalSpecificMonth(final Month start, final Month end) {
-        double total = 0;
-
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            if (start.getValue() <= bankTransaction.getDate().getMonth().getValue()
-                    && bankTransaction.getDate().getMonth().getValue() <= end.getValue()) {
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransactions) ->
+                (start.getValue() <= bankTransactions.getDate().getMonth().getValue()
+                && bankTransactions.getDate().getMonth().getValue() <= end.getValue())
+                ? acc + bankTransactions.getAmount(): acc);
     }
 
     //특정 범주의 입출금 내역 얻기
@@ -64,4 +57,27 @@ public class BankStatementProcessor {
         return total;
     }
 
+    public double summarizeTransactions(BankTransactionSummarizer bankTransactionSummarizer) {
+        double result = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
+        }
+        return result;
+    }
+
+    // functional interface 사용
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() > amount);
+    }
+
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result = new ArrayList<>();
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
 }
